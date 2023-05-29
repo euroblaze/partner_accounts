8b17ab2c87e8c4477c71a60481381b4030aed771
ngkiencuong <85164180+ngkiencuong@users.noreply.github.com>
2023-05-29 15:25:44 +0700

    fix the issue #2

--- models/res_partner.py
+++ models/res_partner.py
@@ -24,43 +24,84 @@ class ResPartner(models.Model):
         help="This account will be used instead of the default one as the payable account for the current partner",
         required=True)
 
-    def action_create_spec_account(self):
-        self.ensure_one()
-        if self.spec_account_receivable_id and self.spec_account_payable_id:
-            raise ValidationError(_("This partner already had the specific accounts!"))
-        return {
-            "name": "Specific Partner Accounts",
-            "type": "ir.actions.act_window",
-            "res_model": "partner.account.wizard",
-            "view_mode": "form",
-            "target": "new",
-        }
-
-    @api.depends('write_date', 'write_uid')
+    @api.depends('spec_account_receivable_id', 'spec_account_payable_id')
     def compute_show_spec_accounts(self):
         for rec in self:
-            rec.show_spec_accounts = True if rec.spec_account_receivable_id and rec.spec_account_payable_id else False
+            rec.show_spec_accounts = True if (rec.spec_account_receivable_id or rec.spec_account_payable_id) else False
 
-    @api.onchange('display_account_payable_id', 'display_account_receivable_id', 'use_specific_accounts')
+    @api.onchange('use_specific_accounts')
     def _onchange_display_accounts(self):
-        if (self.display_account_payable_id or self.display_account_receivable_id) and not self.use_specific_accounts:
+        if not self.use_specific_accounts:
             self.property_account_payable_id = self.display_account_payable_id
             self.property_account_receivable_id = self.display_account_receivable_id
-        elif self.use_specific_accounts:
-            self.property_account_payable_id = self.spec_account_payable_id
-            self.property_account_receivable_id = self.spec_account_receivable_id
-
-    @api.onchange('spec_account_receivable_id', 'spec_account_payable_id')
-    def _onchange_spec_accounts(self):
-        if not self.spec_account_receivable_id and not self.spec_account_payable_id:
-            self.use_specific_accounts = False
-        elif self.spec_account_receivable_id and self.spec_account_payable_id and self.use_specific_accounts:
-            self.property_account_payable_id = self.spec_account_payable_id
-            self.property_account_receivable_id = self.spec_account_receivable_id
+        else:
+            self.property_account_payable_id = self.spec_account_payable_id if self.spec_account_payable_id else self.display_account_payable_id
+            self.property_account_receivable_id = self.spec_account_receivable_id if self.spec_account_receivable_id else self.display_account_receivable_id
 
     @api.model
     def update_display_partner_accounts(self):
         for rec in self.env['res.partner'].sudo().search([]):
-            rec.sudo().with_context(install_update=False).write({
+            rec.sudo().write({
                 'display_account_payable_id': rec.property_account_payable_id,
-                'display_account_receivable_id': rec.property_account_receivable_id})
+                'display_account_receivable_id': rec.property_account_receivable_id
+            })
+
+    def create_spec_account_receivable(self):
+        self.ensure_one()
+        if self.env['account.account'].sudo().search([('spec_partner_receivable', '=', self.id)]):
+            raise ValidationError(_("This partner already had the specific receivable account!"))
+        user_type_id = self.property_account_receivable_id.user_type_id.id if self.property_account_receivable_id \
+            else self.env['account.account.type'].sudo().search([('type', '=', 'receivable')], limit=1).id
+        new_receivable_acc = self.env['account.account'].sudo().with_context(create_spec_account=True).create({
+            'name': self.name,
+            'internal_type': 'receivable',
+            'deprecated': False,
+            'reconcile': True,
+            'spec_partner_receivable': self.id,
+            'user_type_id': user_type_id,
+        })
+        self.sudo().write({
+            'spec_account_receivable_id': new_receivable_acc.id,
+            'property_account_receivable_id': new_receivable_acc.id,
+            'use_specific_accounts': True
+        })
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'message': 'Specific receivable account created successfully!',
+                'type': 'success',
+                'sticky': False,
+                'next': {'type': 'ir.actions.client', 'tag': 'reload'},
+            }
+        }
+
+    def create_spec_account_payable(self):
+        self.ensure_one()
+        if self.env['account.account'].sudo().search([('spec_partner_payable', '=', self.id)]):
+            raise ValidationError(_("This partner already had the specific payable account!"))
+        user_type_id = self.property_account_payable_id.user_type_id.id if self.property_account_payable_id \
+            else self.env['account.account.type'].sudo().search([('type', '=', 'payable')], limit=1).id
+        new_payable_acc = self.env['account.account'].sudo().with_context(create_spec_account=True).create({
+            'name': self.name,
+            'internal_type': 'payable',
+            'reconcile': True,
+            'deprecated': False,
+            'spec_partner_payable': self.id,
+            'user_type_id': user_type_id,
+        })
+        self.sudo().write({
+            'spec_account_payable_id': new_payable_acc.id,
+            'property_account_payable_id': new_payable_acc.id,
+            'use_specific_accounts': True
+        })
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'message': 'Specific payable account created successfully!',
+                'type': 'success',
+                'sticky': False,
+                'next': {'type': 'ir.actions.client', 'tag': 'reload'},
+            }
+        }
